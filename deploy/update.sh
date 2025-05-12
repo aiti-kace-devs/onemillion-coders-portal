71d625230a27882ac3cf491803d031c3ed52b050
Owuraku <boatseth3@gmail.com>
2025-05-12 18:58:00 +0000

    deploy script updated

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,8 +1,7 @@
-cd ../ && git pull \
-&& sudo chown -R $USER:$USER . \
+cd /var/www/html/one-mil-coders && git pull \
 && composer install --no-dev \
 && npm run build \
 && sudo su www-data -s /bin/bash -c 'php artisan horizon:pause && php artisan migrate --force && php artisan optimize:clear && php artisan route:cache && php artisan view:cache && php artisan horizon:continue' \
 && cd deploy \
-&& cat ./fix-permissions.sh | sudo -S bash \
-&& cat ./restart-services.sh | sudo -S bash
+&& cat /var/www/html/one-mil-coders/deploy/fix-permissions.sh | sudo -S bash \
+&& cat /var/www/html/one-mil-coders/deploy/restart-services.sh | sudo -S bash
