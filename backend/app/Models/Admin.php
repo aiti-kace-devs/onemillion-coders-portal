900b51d7966a9c5a7f5c4919eda14dd7de00d628
Owuraku <boatseth3@gmail.com>
2025-07-02 13:18:27 +0000

    added statamic

---
+++ backend/app/Models/Admin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use App\Notifications\ResetPasswordNotification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    protected $guard = 'admin';
+
+    protected $guard_name = 'admin';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['name', 'email', 'password', 'status', 'course_id'];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'preferences' => 'json',
+    ];
+
+    public function isSuper()
+    {
+        return $this->is_super;
+    }
+    public function assignedCourses()
+    {
+        return $this->belongsToMany(Course::class, 'admin_course', 'admin_id', 'course_id')->select('courses.*');
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
+}
---
+++ backend/app/Models/Centre.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Centre extends Model
+{
+    use HasFactory;
+    use \StatamicRadPack\Runway\Traits\HasRunwayResource;
+
+
+    protected $fillable = [
+        'title',
+        'branch_id',
+        'status'
+    ];
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function programme()
+    {
+        return $this->belongsToMany(Programme::class, 'courses');
+    }
+}
