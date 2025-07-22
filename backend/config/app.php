3cb4c28eef7d969ab02c2182c41a5630ee8b07d9
GI-KACE-DEV <dev.aiti.com.gh@gmail.com>
2025-07-22 00:56:43 +0000

    changed app version from 3.5.0 to 3.6.0

--- backend/config/app.php
+++ backend/config/app.php
@@ -237,7 +237,7 @@ return [
 
     ],
 
-    'version' => '3.5.0',
+    'version' => '3.6.0',
     'super_admin_email' => env('SUPER_ADMIN_EMAIL', 'admin@test.com'),
     'super_admin_password' => env('SUPER_ADMIN_PASSWORD', Str::random()),
     'super_admin_name' => env('SUPER_ADMIN_NAME', 'Super Admin'),
