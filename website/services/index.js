4d84615570a932a1f556d3f1b2672f844c242fe6
Ebenezer Sakyi <ebensakyi0@gmail.com>
2025-07-22 08:36:17 +0000

    chore: update package.json dependencies and devDependencies versions

--- website/services/index.js
+++ website/services/index.js
@@ -2,4 +2,4 @@
 export { apiRequest } from './api.js';
  
 // Pages Services
-export { getHomepageData, getGalleryData, getFaqsData, getTestimonialsData, getAboutData, getCategoriesData, getProgrammesData, getPageData } from './pages.js'; 
+export { getHomepageData, getGalleryData, getFaqsData, getTestimonialsData, getAboutData, getCategoriesData, getProgrammesData, getPageData, getCourseRecommendations } from './pages.js'; 
